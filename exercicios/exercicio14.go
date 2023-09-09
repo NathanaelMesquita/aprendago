1ba48ee46a06abe990ce835776d1ea6fbf2335b6
NathanaelMesquita <141948868+NathanaelMesquita@users.noreply.github.com>
2023-09-08 22:21:37 -0300

    Create exercicio14.go

---
+++ exercicios/exercicio14.go
@@ -0,0 +1,15 @@
+//- Crie um loop utilizando a sintaxe: for condition {}
+//- Utilize-o para demonstrar os anos desde que você nasceu.
+//- Solução: https://play.golang.org/p/qnFjiDJzLor
+
+package main
+
+import "fmt"
+
+func main() {
+
+	for x := 1992; x <= 2023; x++ {
+		fmt.Println(x)
+	}
+
+}
