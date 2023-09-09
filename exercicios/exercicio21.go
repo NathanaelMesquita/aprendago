96e7b753d9ca0aa638653d16153ce387a2377f7e
NathanaelMesquita <141948868+NathanaelMesquita@users.noreply.github.com>
2023-09-09 10:19:52 -0300

    Create exercicio21.go

---
+++ exercicios/exercicio21.go
@@ -0,0 +1,13 @@
+//- - Crie um programa que utilize a declaração switch, onde o switch statement seja uma variável do tipo string com identificador "esporteFavorito".
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(true && true)
+	fmt.Println(true && false)
+	fmt.Println(true || true)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+}
