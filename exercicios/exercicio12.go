8c7ed17fc161a5d5e16e1ad4dc1b519de869ab7d
NathanaelMesquita <141948868+NathanaelMesquita@users.noreply.github.com>
2023-09-08 21:25:29 -0300

    Create exercicio12.go

---
+++ exercicios/exercicio12.go
@@ -0,0 +1,13 @@
+// - Põe na tela: todos os números de 1 a 10000.
+// - Solução: https://play.golang.org/p/MkdZiDW8SQ
+package main
+
+import "fmt"
+
+func main() {
+
+	for x := 1; x <= 1000; x++ {
+		fmt.Println(x)
+	}
+
+}
