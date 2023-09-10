0b930234e946bfab6a4aa0b228e68aeeca15047c
NathanaelMesquita <141948868+NathanaelMesquita@users.noreply.github.com>
2023-09-10 17:45:57 -0300

    Create exercicio36.go

---
+++ exercicios/exercicio36.go
@@ -0,0 +1,16 @@
+//- Crie e use um struct anônimo.
+//- Desafio: dentro do struct tenha um valor de tipo map e outro do tipo slice.
+
+package main
+
+import "fmt"
+
+type anonimo struct{
+	mapa map[string][]string,
+	slice []string
+
+}
+
+func main() {
+	
+}
