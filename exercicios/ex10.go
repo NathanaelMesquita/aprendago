fc4cb0707c339e43e04e073750cbf23947f2f47a
NathanaelMesquita <141948868+NathanaelMesquita@users.noreply.github.com>
2023-09-13 13:35:39 -0300

    comiit

---
+++ exercicios/ex10.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	x := `literal string 
+	
+	testando essa função`
+	fmt.Println(x)
+
+}
---
+++ exercicios/ex11.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	x := 1992
+	for x < 2024 {
+		fmt.Println(x)
+		x++
+	}
+
+}
