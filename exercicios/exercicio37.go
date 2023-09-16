53fd673e7193b8c3dc182ccf117315f54596e4bd
NathanaelMesquita <141948868+NathanaelMesquita@users.noreply.github.com>
2023-09-16 11:35:03 -0300

    Create exercicio37.go

---
+++ exercicios/exercicio37.go
@@ -0,0 +1,15 @@
+//- Crie e use um struct anônimo.
+//- Desafio: dentro do struct tenha um valor de tipo map e outro do tipo slice.
+
+package main
+
+import "fmt"
+
+func main() {
+	x := 1250
+	fmt.Println(x)
+
+	y := &x
+	fmt.Println("Esse é o endereço de x", y)
+	fmt.Println("Esse é o valor de x, usando ponteiro", *x)
+}
