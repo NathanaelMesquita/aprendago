ab6a0155eaee8a8fd30c5a7468b3c2f319c393a5
NathanaelMesquita <141948868+NathanaelMesquita@users.noreply.github.com>
2023-09-19 17:55:36 -0300

    Create ex43.go

---
+++ exercicios/ex43.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+type Pessoa struct{
+			Nome string,
+			Sobrenome string,
+			Sabores[]string ,
+
+}
+
+func main() {
+	
+pessoa1 := pessoa{"Nathanael", "Mesquita", []string = "chocolate", "morango", "baunilha"}
+
+pessoa2 := pessoa{"Mariana", "Costa", []string = "flocos", "brigadeiro", "napolitano"}
+
+for i, n := range pessoa{
+		fmt.Println(b)
+		for j, s := range Sabores{
+			fmt.Println(j,"-",s)
+		}
+
+}
+
+
+	
+}
